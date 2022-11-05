809f6bbf464a460f3f5d7e478eb4962f672aecdc
billto32 <107520575+billto32@users.noreply.github.com>
2022-11-05 00:05:07 -0400

    Explanatory analysis of SQL queries

--- HR Related Works/HRAttrition.sql
+++ HR Related Works/HRAttrition.sql
@@ -92,20 +92,69 @@ alter column yearssincelastpromotion type int
 alter column yearswithcurrmanager type int
 	using yearswithcurrmanager::integer
 ;
+with a as (
+	select gender , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by gender),
+b as (
+	select gender , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by gender)
+select a.gender, a.attrition, b.retention from a
+inner join b 
+	on a.gender = b.gender
+group by a.gender ,a.attrition, b.retention
+;
+/* 13.78% attrition vs 17% attrition rate, minor difference between genders.*/
 
-select gender , count(attrition), count(employeecount) from hrattrition
-where attrition = 'Yes'
-group by gender
+
+with a as (
+		select 
+			sum(
+				case when age <20 then 1 else 0
+			end) as "<20",
+			sum(
+				case when age between 21 and 30 then 1 else 0
+			end) as "21-30",
+			sum(
+				case when age between 31 and 40 then 1 else 0
+			end) as "31-40",
+			sum(
+				case when age between 40 and 50 then 1 else 0
+			end) as "41-50",
+			sum(
+				case when age >50 then 1 else 0
+			end) as ">50", 
+			count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+),
+b as (select 
+			sum(
+				case when age <20 then 1 else 0
+			end) as "<20",
+			sum(
+				case when age between 21 and 30 then 1 else 0
+			end) as "21-30",
+			sum(
+				case when age between 31 and 40 then 1 else 0
+			end) as "31-40",
+			sum(
+				case when age between 40 and 50 then 1 else 0
+			end) as "41-50",
+			sum(
+				case when age >50 then 1 else 0
+			end) as ">50", 
+			count(attrition) as retention from hrattrition
+	where attrition = 'No'
+)
+select * from a,b
 ;
-select gender , attrition from hrattrition
-where gender = 'Female'
-;
-select age, count(attrition) as attrition from hrattrition
+select age, count(age) as total_employees from hrattrition
 group by age
-order by attrition desc
-;
-select employeenumber , attrition from public.hrattrition
-where attrition = 'Yes'
+order by total_employees desc;
+/*Most employee turnover rate occurs before 40. Employees start to settle down once they 
+look for stability in the career. Employees at a younger age are more likely to move to different
+companies possibly better salaries or advancement in their careers.*/
 
 select 
 		employeenumber , 
@@ -118,15 +167,189 @@ select
 from public.hrattrition
 where attrition = 'Yes'
 ;
-select jobsatisfaction, attrition, count(jobsatisfaction) from public.hrattrition
-group by jobsatisfaction, attrition
-order by jobsatisfaction, attrition
+--Survey Evaluations
+with a as (
+	select jobsatisfaction , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by jobsatisfaction),
+b as (
+	select jobsatisfaction , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by jobsatisfaction)
+select a.jobsatisfaction, a.attrition, b.retention from a
+inner join b 
+	on a.jobsatisfaction = b.jobsatisfaction
+group by a.jobsatisfaction ,a.attrition, b.retention
 
-select environmentsatisfaction, count(environmentsatisfaction) from hrattrition
-where attrition = 'Yes'
-group by environmentsatisfaction
+/* Employees may look for better opportunities however, a large portion still stay within the company
+even while unsatisfied. Attrition rate decreases as employees are more satisfied. Similar trends
+through other survey evaluations*/
 
+with a as (
+	select environmentsatisfaction , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by environmentsatisfaction),
+b as (
+	select environmentsatisfaction , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by environmentsatisfaction)
+select a.environmentsatisfaction, a.attrition, b.retention from a
+inner join b 
+	on a.environmentsatisfaction = b.environmentsatisfaction
+group by a.environmentsatisfaction ,a.attrition, b.retention
+order by attrition desc
+;
+/* Room for improvement in work environement */
+with a as (
+	select worklifebalance , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by worklifebalance),
+b as (
+	select worklifebalance , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by worklifebalance)
+select a.worklifebalance, a.attrition, b.retention from a
+inner join b 
+	on a.worklifebalance = b.worklifebalance
+group by a.worklifebalance ,a.attrition, b.retention
+order by attrition desc
+;
+/* Those with poor work life balance most likely adjusted  to the job type. Once accustomed to the
+work, they are more likely to find and explore more opportunities with more free time. Those who
+are satisfied settle down */
 
+with a as (
+	select relationshipsatisfaction , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by relationshipsatisfaction),
+b as (
+	select relationshipsatisfaction , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by relationshipsatisfaction)
+select a.relationshipsatisfaction, a.attrition, b.retention from a
+inner join b 
+	on a.relationshipsatisfaction = b.relationshipsatisfaction
+group by a.relationshipsatisfaction ,a.attrition, b.retention
+order by attrition desc
+;
+/* Those with satisfactorily relationships look for better career opportunities while those with 
+poor relationships stick to work. */
 
+with a as (
+	select jobinvolvment , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by jobinvolvment),
+b as (
+	select jobinvolvment , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by jobinvolvment)
+select a.jobinvolvment, a.attrition, b.retention from a
+inner join b 
+	on a.jobinvolvment = b.jobinvolvment
+group by a.jobinvolvment ,a.attrition, b.retention
+order by attrition desc
+;
 
+--Length of Time Measurements
+with a as (
+	select yearsincurrentrole , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by yearsincurrentrole),
+b as (
+	select yearsincurrentrole , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by yearsincurrentrole)
+select a.yearsincurrentrole, a.attrition, b.retention from a
+inner join b 
+	on a.yearsincurrentrole = b.yearsincurrentrole
+group by a.yearsincurrentrole ,a.attrition, b.retention
+order by attrition desc
+;
+/*Those who starting early in their careers are more likely to leave the company. Looking for more
+experience, and salary hikes. */
 
+with a as (
+	select yearsatcompany , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by yearsatcompany),
+b as (
+	select yearsatcompany , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by yearsatcompany)
+select a.yearsatcompany, a.attrition, b.retention from a
+inner join b 
+	on a.yearsatcompany = b.yearsatcompany
+group by a.yearsatcompany ,a.attrition, b.retention
+order by attrition desc
+;
+/* Likewise, we see a similar trend of those early on the company leaving for more opportunities. 
+Possible relationships between managers. */
+
+with a as (
+	select yearswithcurrmanager , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by yearswithcurrmanager),
+b as (
+	select yearswithcurrmanager , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by yearswithcurrmanager)
+select a.yearswithcurrmanager, a.attrition, b.retention from a
+inner join b 
+	on a.yearswithcurrmanager = b.yearswithcurrmanager
+group by a.yearswithcurrmanager ,a.attrition, b.retention
+order by attrition desc
+;
+/* Employees are unlikely to resign due to managers. Depicted earlier, employees are more likely to leave
+early on in their careers. However, there is a spike with employees stay 7 years. Still, the employees
+are most likely satisfied with their managers after staying for so long. */
+
+--Salary Influences
+with a as (
+	select yearssincelastpromotion , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by yearssincelastpromotion),
+b as (
+	select yearssincelastpromotion , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by yearssincelastpromotion)
+select a.yearssincelastpromotion, a.attrition, b.retention from a
+inner join b 
+	on a.yearssincelastpromotion = b.yearssincelastpromotion
+group by a.yearssincelastpromotion ,a.attrition, b.retention
+order by attrition desc
+;
+/* Large decrease in attrition rates after employees first year of promotion, employees
+potentially satisfied with salaries to stay at company. */
+
+with a as (
+	select stockoptionlevel , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by stockoptionlevel),
+b as (
+	select stockoptionlevel , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by stockoptionlevel)
+select a.stockoptionlevel, a.attrition, b.retention from a
+inner join b 
+	on a.stockoptionlevel = b.stockoptionlevel
+group by a.stockoptionlevel ,a.attrition, b.retention
+order by attrition desc
+;
+/* Providing stock options increases incentitive to stay at the company for several more years. Large
+amounts of money in make employees stay. Those without this opportunity most likely will leave. */
+
+with a as (
+	select percentsalaryhike , count(attrition) as attrition from hrattrition
+	where attrition = 'Yes'
+	group by percentsalaryhike),
+b as (
+	select percentsalaryhike , count(attrition) as retention from hrattrition
+	where attrition = 'No'
+	group by percentsalaryhike)
+select a.percentsalaryhike, a.attrition, b.retention from a
+inner join b 
+	on a.percentsalaryhike = b.percentsalaryhike
+group by a.percentsalaryhike ,a.attrition, b.retention
+order by attrition desc
+;
+/* Similarly, large salary hikes influences employees to stay at the company.*/
--- SQL Exploration/DVD Rental/dvdrental.sql
+++ SQL Exploration/DVD Rental/dvdrental.sql
@@ -34,8 +34,15 @@ with t1 as (
 select t1.genre, total_customers, total_sales from t1
 inner join t2
 	on t1.genre = t2.genre
+--order by total_sales desc	
 ;
+/* Consumers are more likely to rent out action orientated films (Sports, Animation, Action, Sci-Fi,
+Family. Possible idea of adrenaline as a reason for more rentals but, Horror films is one of the 
+least rented films. However, with an order by total_sales desc we find that Sports, Sci-Fi, Animation,
+Drama, and Comedy are the most profitable. Possibly cheaper production cost, lower quality of films
+or higher varity of films effecting prices that drop Family and Action film sales. */
 
+--How does rating effect the amount of films sold?
 select title , rating, count(i.film_id) as total_rental from film f
 inner join inventory i
 	on f.film_id = i.film_id
@@ -53,7 +60,10 @@ inner join rental r
 group by rating
 order by total_rental desc
 ;
+/*High audience within PG-13 and lower,possibly more likely to rent out films are younger kids or
+parents with teens & children.*/
 
+--Where are rentals coming from?
 select country, count(*) as total_customers, sum(amount) as total_sales from country c
 inner join city ct
 	on c.country_id = ct.country_id
@@ -66,6 +76,16 @@ inner join payment p
 group by country
 order by total_sales desc
 ;
+/* India, China, US are top 3 consumers of dvd rentals. Large difference between Asia's consumption
+of dvd rentals compared to European and North American consumed. Population of countries 
+effecting the rate of dvd rentals? How is shipping and logistics of dvd rentals worth sending 
+dvd rentals to certain countries? Possible reasons for lower dvd rentals include shipping, origin
+of film, language, pricing, quality of film. */
+select lang.name, count(lang.name) as language_total from film f
+inner join language lang
+	on f.language_id = lang.language_id
+group by lang.name;
+-- Only English rentals available
 
 select 
 		store_id,
@@ -77,9 +97,9 @@ inner join inventory i
 	on r.inventory_id = i.inventory_id
 group by store_id
 ;
+-- Physical stores perform nearly identical
 
-/*Rental Return Times */
-
+--Rental Return Times 
 with dd as 
 	(select 
 	 	*,
@@ -102,6 +122,10 @@ rd as
 select return_status , count(*) from rd
 group by return_status
 ;
+/* 59% of rentals are returned early or on time. Reasonings for late rentals? Determine through 
+tracking labels, and customer surveys. Establish fees to deter late rentals while creating another 
+source of revenue. Introduction of rewards for repeating customers within good standing 
+establishing customer loyalty. */
 
 select name, email, total_payment from
 	(select 
